305fbf6621ad869df86b8712c02db778f5f39f25
Freeman Robert Allan Schlichting <freeman7schlichting@gmail.com>
2018-06-06 13:44:48 -0500

    First question

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@
 
 
 var name = prompt("What is your name?");
-var name = prompt("Hello " + name + "!!");
+var question = prompt("Hello " + name + "!!  " +
+    "Do you think Freeman plays the piano?");
+var answer = question.toUpperCase().trim();
 
+if (answer === "YES" || answer === "Y") {
+  alert(answer + ", That is correct, well at least I use to.")
+} else if (answer === "NO" || answer === "N") {
+  alert(answer + ", I don't currently play much, but I use to.")
+}
 
